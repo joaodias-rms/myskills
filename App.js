b190fec65ee1e8b3e68819e8ac10193bd9b2fee7
joao <jvd.ramos@hotmail.com>
2021-10-28 14:49:33 -0400

    ajuste na statusbar

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-import {View} from 'react-native';
+import {StatusBar} from 'react-native';
 import {Home} from './src/Screens/Home';
-import {styles} from './styles';
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <Home />
+    </>
+  );
 }
