c7d5838515110af66e6a706d21dd275a3cf72af5
joao <joao.victor@topsapp.com.br>
2022-03-08 15:56:02 -0400

    refatorado para testes com detox

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,6 +1,16 @@
 import React, {useEffect, useState} from 'react';
 
-import {View, Text, TextInput, FlatList, Alert} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  FlatList,
+  Alert,
+  KeyboardAvoidingView,
+  Keyboard,
+  Platform,
+  TouchableWithoutFeedback,
+} from 'react-native';
 
 import {Button} from '../../Components/Button';
 import {SkillCard} from '../../Components/SkillCard';
@@ -47,31 +57,40 @@ export function Home() {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome, boy</Text>
-      <Text style={styles.greetings}>{greeting}</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="New Skill"
-        placeholderTextColor="#555"
-        onChangeText={setNewSkill}
-      />
-      <Button onPress={handleAddNewSkill} title="Add" />
-
-      <Text style={[styles.title, {marginVertical: 50}]}>My Skills</Text>
-
-      <FlatList
-        data={mySkills}
-        keyExtractor={item => item.id}
-        renderItem={({item}) => (
-          <SkillCard
-            skill={item.name}
-            onPress={() => {
-              handleRemoveSkill(item.id);
-            }}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <>
+          <Text style={styles.title} testID="welcome">
+            Welcome, boy
+          </Text>
+          <Text style={styles.greetings}>{greeting}</Text>
+          <TextInput
+            testID="input-new"
+            style={styles.input}
+            placeholder="New Skill"
+            placeholderTextColor="#555"
+            onChangeText={setNewSkill}
           />
-        )}
-      />
-    </View>
+          <Button onPress={handleAddNewSkill} title="Add" testID="button-add" />
+
+          <Text style={[styles.title, {marginVertical: 50}]}>My Skills</Text>
+
+          <FlatList
+            data={mySkills}
+            keyExtractor={item => item.id}
+            renderItem={({item}) => (
+              <SkillCard
+                skill={item.name}
+                onPress={() => {
+                  handleRemoveSkill(item.id);
+                }}
+              />
+            )}
+          />
+        </>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 }
